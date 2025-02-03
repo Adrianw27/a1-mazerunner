736c3aec1050939ff5597d74c84176d6d73a348d
Adrian <adrian.winter27@gmail.com>
2025-02-03 02:25:53 -0500

    Removed Logger

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,15 +3,11 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.*;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        logger.info("** Starting Maze Runner");
 
         // Setup command-line option: -i
         Option inputFileOption = Option.builder("i")
@@ -28,7 +24,7 @@ public class Main {
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
-            logger.error("An error has occurred while parsing command line options", e);
+            System.exit(0);
         }
 
         // Get file path
@@ -37,18 +33,14 @@ public class Main {
         char[][] mazeData = parseMaze(inputFilePath);
         Maze maze = new Maze(mazeData);
 
-        logger.info("**** Computing path");
-
         List<Character> path = MazeSolver.findAnyPath(mazeData, maze.getEntry(), maze.getExit());
         if (path.isEmpty()) {
-            logger.info("No path found using right-hand exploration.");
+            System.out.println("No path found.");
         } else {
-            logger.info("Path using right-hand exploration:");
             String factorizedPath = MazeSolver.factorizePath(path);
             System.out.println(factorizedPath);
         }
 
-        logger.info("** End of MazeRunner");
     }
 
     /**
@@ -59,22 +51,13 @@ public class Main {
         List<char[]> rows = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
-            logger.info("**** Reading the maze from file {}", inputFilePath);
             String line;
             while ((line = reader.readLine()) != null) {
                 char[] lineChars = line.toCharArray();
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        logger.debug("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        logger.debug("PASS ");
-                    }
-                }
-                logger.debug(System.lineSeparator());
                 rows.add(lineChars);
             }
         } catch (Exception e) {
-            logger.error("An error has occurred while reading the maze file", e);
+            System.exit(0);
         }
 
         return rows.toArray(new char[0][]);
